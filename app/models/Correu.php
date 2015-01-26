66eb1bafcb6b372a5b592e3cacfccf8a35467362
sacer6 <jonatan.d.g@hotmail.com>
2015-01-26 18:53:12 +0100

    IDs en archivos

--- app/models/Correu.php
+++ app/models/Correu.php
@@ -5,7 +5,7 @@ use SleepingOwl\Models\SleepingOwlModel;
 class Correu extends SleepingOwlModel{
         protected $table = "correus";
 	protected $fillable = ['correus'];
-	protected $hidden = ['created_at','updated_at'];
+	protected $hidden = ['id','id_usuari','created_at','updated_at'];
 
 	public function scopeDefaultSort($query){
 		return $query->orderBy('contingut', 'asc');
--- app/models/Model.php
+++ app/models/Model.php
@@ -5,7 +5,7 @@ use SleepingOwl\Models\SleepingOwlModel;
 class Model extends SleepingOwlModel{
         protected $table = "models";
 	protected $fillable = ['models'];
-	protected $hidden = ['created_at','updated_at'];
+	protected $hidden = ['id','id_marca','created_at','updated_at'];
 
 	public function scopeDefaultSort($query){
 		return $query->orderBy('id', 'asc');
