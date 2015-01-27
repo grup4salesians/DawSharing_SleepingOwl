ab152845a8666ea08a7e43bbc739737de4c58a50
sacer6 <jonatan.d.g@hotmail.com>
2015-01-27 20:54:48 +0100

    correu PUTO ARNAU!!

--- app/admin/correu.php
+++ app/admin/correu.php
@@ -3,13 +3,15 @@
 Admin::model(\Correu::Class)->title('Correu')->columns(function (){
     	//Column::string('id', 'id');	
         //Column::string('id_usuari', 'id_usuari');
-        Column::string('usuari.nom', 'Usari');
+        Column::string('usuari.nom', 'Usuari Envia');
+        Column::string('usuariDest.nom', 'Destinatari');
 	Column::string('contingut', 'Contingut');
         Column::string('vist', 'Vist');
         
 })->form(function (){
         //FormItem::text('id', 'id');	
-        FormItem::select('id_usuari', 'Usuari')->list(\Usuari::class)->required();
+        FormItem::select('id_usuari', 'Usari Envia')->list(\Usuari::class)->required();
+        FormItem::select('id_destinatari', 'Destinatari')->list(\Usuari::class)->required();
 	FormItem::ckeditor('contingut', 'Contingut');
         FormItem::checkbox('vist', 'Vist');
 
--- app/models/Correu.php
+++ app/models/Correu.php
@@ -5,7 +5,7 @@ use SleepingOwl\Models\SleepingOwlModel;
 class Correu extends SleepingOwlModel{
         protected $table = "correus";
 	
-	protected $hidden = ['id','id_usuari','created_at','updated_at'];
+	protected $hidden = ['id','id_usuari','id_destinatari','created_at','updated_at'];
 
 	public function scopeDefaultSort($query){
 		return $query->orderBy('contingut', 'asc');
@@ -13,6 +13,9 @@ class Correu extends SleepingOwlModel{
         
         public function usuari(){
             return $this->belongsTo('Usuari','id_usuari');
+        }
+        public function usuariDest(){
+            return $this->belongsTo('Usuari','id_destinatari');
         }
 
 }
