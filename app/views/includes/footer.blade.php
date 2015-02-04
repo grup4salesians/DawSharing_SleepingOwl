ed3193362120ecfcd7630711780d38ac7a402f1d
sacer6 <jonatan.d.g@hotmail.com>
2015-02-04 19:19:04 +0100

    poniendo cosas bien

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,7 +1,5 @@
-<div class="fotter">
-	 <div class="container">
-		 <h3>143 City Located Hotels World Wide</h3>
-		 <h4>"Hospitality, Quality & Good Locations. We only provide you with the best hotels" - John Deo</h4>
-		 <i class="man"></i>
-	 </div>
+<div class="container">
+    <h3>143 City Located Hotels World Wide</h3>
+    <h4>"Hospitality, Quality & Good Locations. We only provide you with the best hotels" - John Deo</h4>
+    <i class="man"></i>
 </div>
--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,41 +1,36 @@
-<div class="header">
-	 <div class="top-header">
-		 <div class="container">
-			 <div class="logo">
-				 	<!--<a href="index.html"><img src="images/logo.png"/></a>-->
-					
-			 </div>
-			 <span class="menu"> </span>
-			 <div class="m-clear"></div>
-			 <div class="top-menu">
-				<ul>
-				<!--
-					<li class="active"><a href="index.html">START</a></li>
-					<li><a class="scroll" href="facilities.html">FACILITIES</a></li>
-				-->
-				<li><a class="scroll" href="facilities.html">Registra't</a></li>
-				<li><a class="scroll" href="facilities.html">Usuaris</a></li>
-				<li>
-				    <select id="country" onchange="change_country(this.value)" class="frm-field required">
-						<option value="null">CA</option>
-						<option value="null">ES</option>         
-						<option value="AX">EN</option>
-					</select>
-				</li>
-				</ul>
-				<script>
-					$("span.menu").click(function(){
-						$(".top-menu ul").slideToggle(200);
-					});
-				</script>
-			 </div>
-			 <div class="clearfix"></div>
-		  </div>
-	  </div>
-	  <div class="banner">
-			<div class="banner-info text-center">
-				<h1>DawSharing</h1>
-				<span></span>
-			</div>
-	  </div>
+<div class="top-header">
+    <div class="container">
+        <div class="logo">
+                       <!--<a href="index.html"><img src="images/logo.png"/></a>-->
+        </div>
+        <span class="menu"> </span>
+        <div class="m-clear"></div>
+        <div class="top-menu">
+            <ul>
+                <!--    <li class="active"><a href="index.html">START</a></li>
+                        <li><a class="scroll" href="facilities.html">FACILITIES</a></li>    -->
+                <li><a class="scroll" href="facilities.html">Registra't</a></li>
+                <li><a class="scroll" href="facilities.html">Usuaris</a></li>
+                <li>
+                    <select id="country" onchange="change_country(this.value)" class="frm-field required">
+                        <option value="null">CA</option>
+                        <option value="null">ES</option>         
+                        <option value="AX">EN</option>
+                    </select>
+                </li>
+            </ul>
+            <script>
+                $("span.menu").click(function () {
+                    $(".top-menu ul").slideToggle(200);
+                });
+            </script>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<div class="banner">
+    <div class="banner-info text-center">
+        <h1>Troba companys per viatjar</h1>
+        <span></span>
+    </div>
 </div>
