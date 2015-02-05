96d464c5210984863e90692e2fa1d74a7a7da439
Vektron <kenzo_aku@hotmail.com>
2015-02-05 20:37:52 +0100

    Login

--- app/models/Usuari.php
+++ app/models/Usuari.php
@@ -1,17 +1,24 @@
 <?php
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
 use SleepingOwl\Models\Interfaces\ModelWithImageFieldsInterface;
 use SleepingOwl\Models\SleepingOwlModel;
 use SleepingOwl\Models\Traits\ModelWithImageOrFileFieldsTrait;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class Usuari extends SleepingOwlModel implements ModelWithImageFieldsInterface {
+class Usuari extends SleepingOwlModel implements ModelWithImageFieldsInterface, UserInterface, RemindableInterface {
 
+    use UserTrait, RemindableTrait;
     use ModelWithImageOrFileFieldsTrait;
 
     protected $table = "usuaris";
     protected $fillable = ['nom', 'cognoms', 'dni', 'grup_escolar', 'foto', 'correu', 'rol', 'fecha_inscripcion', 'contrasenya', 'sexe', 'data_naixement', 'idioma'];
-
+    
+    protected $hidden = array('contrasenya', 'remember_token');
+    
     public function scopeDefaultSort($query) {
         return $query->orderBy('nom', 'asc');
     }
@@ -31,9 +38,13 @@ class Usuari extends SleepingOwlModel implements ModelWithImageFieldsInterface {
     public function getDates() {    
         return array_merge(parent::getDates(), ['data_naixement','fecha_inscripcion']);
     }
-//    public function getTimeStamps() {    
-//        return array_merge(parent::getTimeStamps(), ['fecha_inscripcion']);
-//    }
-    
 
+
+    public function getAuthIdentifier() {
+        return $this->id;
+    }
+
+    public function getAuthPassword() {
+        return Hash::make($this->contrasenya);
+    }
 }
--- app/routes.php
+++ app/routes.php
@@ -11,13 +11,31 @@
 |
 */
 
-Route::get('/', function(){
+/*Route::get('/', function(){
     return View::make('pages.home');
 });
 
 Route::get('login', function(){
     return Redirect::to('/');
     //return View::make('pages.login');
+});*/
+
+// Nos mostrará el formulario de login.
+Route::get('login', 'AuthController@showLogin');
+
+// Validamos los datos de inicio de sesión.
+Route::post('login', 'AuthController@postLogin');
+
+// Nos indica que las rutas que están dentro de él sólo serán mostradas si antes el usuario se ha autenticado.
+Route::group(array('before' => 'auth'), function()
+{
+    // Esta será nuestra ruta de bienvenida.
+    Route::get('/', function()
+    {
+        return View::make('pages.home');
+    });
+    // Esta ruta nos servirá para cerrar sesión.
+    Route::get('logout', 'AuthController@logOut');
 });
 
 Route::get('registre', function(){
