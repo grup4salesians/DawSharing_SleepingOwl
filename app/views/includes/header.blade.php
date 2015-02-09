e75275d9effa92f3d2c21d12bd3b8beef7272d89
sacer6 <jonatan.d.g@hotmail.com>
2015-02-09 19:41:49 +0100

    login y demas parte 2

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -7,24 +7,50 @@
         <div class="m-clear"></div>
         <div class="top-menu">
             <ul>
-                 <!--   <li class="active"><a href="index.html">START</a></li>
-                        <li><a class="scroll" href="facilities.html">FACILITIES</a></li> 
-                   -->
-                              
-                <li>
+                <!--   <li class="active"><a href="index.html">START</a></li>
+                       <li><a class="scroll" href="facilities.html">FACILITIES</a></li> 
+                -->
 
-    		<a href="#" data-toggle="modal" data-target=".bs-example-modal-sm">Inicia Sessió</a>
+                <?php if (Auth::check()) { ?>
+                    <li role="presentation" class="dropdown">
+                        <a id="dLabel" href="#" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false" style="color:white;">
+                             <?php echo Auth::user()->getFullNameAttribute(); ?> <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                            <li role="presentation">
+                                <a role="menuitem" tabindex="-1" href="#">Els meus viatges</a>
+                            </li>
+                            <li role="presentation">
+                                <a role="menuitem" tabindex="-1" href="#">Missatges</a>
+                            </li>
+                            <li role="presentation">
+                                <a role="menuitem" tabindex="-1" href="#">Perfil</a>
+                            </li>
+                            <li role="presentation">
+                                <a role="menuitem" tabindex="-1" href="#">Ajuda</a>
+                            </li>
+                            <li role="presentation" class="divider"></li>
+                            <li role="presentation">
+                                <a role="menuitem" tabindex="-1" href="logout">Sortir</a>
+                            </li>
+                        </ul>
 
-<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content" style="height: 200px;">
-        @include('includes.login')
-    </div>
-  </div>
-</div>
- 
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <a href="#" data-toggle="modal" data-target=".bs-example-modal-sm">Inicia Sessió</a>
 
-                </li>
+                        <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-sm">
+                                <div class="modal-content" style="height: 200px;">
+                                    @include('includes.login')
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                <?php } ?>   
+
+
                 <!--<li><a class="scroll" href="facilities.html"></a></li>-->
                 <li><a class="scroll" href="facilities.html">Usuaris</a></li>
                 <li>
