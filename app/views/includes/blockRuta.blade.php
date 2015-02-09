bd396400059c3db8b369cdd6efcc53bb00f1d63a
Vektron <kenzo_aku@hotmail.com>
2015-02-09 20:55:27 +0100

    A la mitad, objeto de la ruta

---
+++ app/views/includes/blockRuta.blade.php
@@ -0,0 +1,39 @@
+<?php
+
+	public class blockRuta(){
+
+		private $id, $data, $rutaInici, $rutaFi, $preu, $seientsRestants, $permisos;
+
+		public __construct($id = 0, $data = null, $rutaInici = "", $rutaFi = "", $preu = 99999, $seientsRestants = 0, $permisos = "") {
+			$this->id = $id; 
+			$this->data = $data; 
+			$this->rutaInici = $rutaInici; 
+			$this->rutaFi = $rutaFi; 
+			$this->preu = $preu; 
+			$this->seientsRestants = $seientsRestants; 
+			$this->permisos = $permisos;
+		}
+
+		public function mostrarMapa() {
+			$code = "";
+
+
+
+			return $code;
+		}
+
+		public function __get ($property) {
+			if (property_exists($this, $property)) {
+				return $this->$property;
+			}
+		}
+		public function __set ($property, $value) {
+			if (property_exists($this, $property)) {
+				$this->$property = $value;
+			}
+			return $this;
+		}
+
+	}
+
+?>
