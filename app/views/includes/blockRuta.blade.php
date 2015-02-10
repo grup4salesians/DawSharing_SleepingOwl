b76887e293d607fbcd415b238a80e0c68c71bbc8
sacer6 <jonatan.d.g@hotmail.com>
2015-02-10 16:23:17 +0100

    clase blockRuta bien echa

--- app/views/includes/blockRuta.blade.php
+++ app/views/includes/blockRuta.blade.php
@@ -1,39 +1,40 @@
 <?php
 
-	public class blockRuta(){
+class blockRuta {
 
-		private $id, $data, $rutaInici, $rutaFi, $preu, $seientsRestants, $permisos;
+    private $id, $data, $rutaInici, $rutaFi, $preu, $seientsRestants, $permisos;
 
-		public __construct($id = 0, $data = null, $rutaInici = "", $rutaFi = "", $preu = 99999, $seientsRestants = 0, $permisos = "") {
-			$this->id = $id; 
-			$this->data = $data; 
-			$this->rutaInici = $rutaInici; 
-			$this->rutaFi = $rutaFi; 
-			$this->preu = $preu; 
-			$this->seientsRestants = $seientsRestants; 
-			$this->permisos = $permisos;
-		}
+    public function __construct($id = 0, $data = null, $rutaInici = "", $rutaFi = "", $preu = 99999, $seientsRestants = 0, $permisos = "") {
+        $this->id = $id;
+        $this->data = $data;
+        $this->rutaInici = $rutaInici;
+        $this->rutaFi = $rutaFi;
+        $this->preu = $preu;
+        $this->seientsRestants = $seientsRestants;
+        $this->permisos = $permisos;
+    }
 
-		public function mostrarMapa() {
-			$code = "";
+    public function mostrarMapa() {
+        $code = "";
 
 
 
-			return $code;
-		}
+        return $code;
+    }
 
-		public function __get ($property) {
-			if (property_exists($this, $property)) {
-				return $this->$property;
-			}
-		}
-		public function __set ($property, $value) {
-			if (property_exists($this, $property)) {
-				$this->$property = $value;
-			}
-			return $this;
-		}
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
 
-	}
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this;
+    }
+
+}
 
 ?>
