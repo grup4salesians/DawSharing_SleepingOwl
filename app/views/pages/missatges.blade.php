6b1f459286d5a09651b1b36aa95639ef6d3b4273
sacer6 <jonatan.d.g@hotmail.com>
2015-02-18 16:27:19 +0100

    correu parte 3 modificacion header

--- app/views/pages/missatges.blade.php
+++ app/views/pages/missatges.blade.php
@@ -10,8 +10,11 @@ Mails
 <div class="rooms text-center">
             <p />
     <div class="BandejaMails_Main">
-
-         <?php $missatges = Correu::orderBy('id', 'desc')->get(); ?>
+        <div class="BadenjaMails_Header">        
+            <i class='fa fa-trash fa-3x'></i>
+        </div>
+        
+         <?php $missatges = Correu::where('usuari_id',Auth::user()->id)->orderBy('id', 'desc')->get(); ?>
             @foreach($missatges as $key => $missat)
                     <?php
                         $missatge = new email($missat->id, '15/02/2015', $missat->usuari_id,  $missat->assumpte,substr($missat->contingut,0,100),$missat->vist);
