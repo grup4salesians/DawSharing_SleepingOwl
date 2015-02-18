f0be7de3d9b92b7a3238c5671f2fbe1922229bf3
Vektron <kenzo_aku@hotmail.com>
2015-02-18 17:31:07 +0100

    man

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
+    <MARQUEE style="height: 277px;display: block;position: absolute;top: -84%;"><i class="man"></i></MARQUEE>
 <div class="container">
     <h3>DawSharing per a tot salesià!</h3>
     <h4>"Hospitalitat,qualitat, feedback, economic i divertit!" - GRUP4</h4>
-    <i class="man"></i>
 </div>
--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -4,22 +4,22 @@
 <link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css'>
 <!--<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>-->
 
-<link href="/DawSharing_SleepingOwl/public/css/style.css" rel="stylesheet" type="text/css" media="all">
+<link href="/Projectes/01/DawSharing_SleepingOwl/public/css/style.css" rel="stylesheet" type="text/css" media="all">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--<script src="js/jquery.min.js"></script>-->
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script src="/DawSharing_SleepingOwl/public/js/jquery-ui.js"></script>
-<link rel="stylesheet" href="/css/jquery-ui.css" >
+<script src="/Projectes/01/DawSharing_SleepingOwl/public/js/jquery-ui.js"></script>
+<link rel="stylesheet" href="/Projectes/01/css/jquery-ui.css" >
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-<script type="text/javascript" src="/DawSharing_SleepingOwl/public/js/JFForms.js"></script>
-<script type="text/javascript" src="/DawSharing_SleepingOwl/public/js/JFCore.js"></script>
-<link type="text/css" rel="stylesheet" href="/DawSharing_SleepingOwl/public/css/JFFormStyle-1.css">
-<link type="text/css" rel="stylesheet" href="/DawSharing_SleepingOwl/public/css/JFGrid.css" />
+<script type="text/javascript" src="/Projectes/01/DawSharing_SleepingOwl/public/js/JFForms.js"></script>
+<script type="text/javascript" src="/Projectes/01/DawSharing_SleepingOwl/public/js/JFCore.js"></script>
+<link type="text/css" rel="stylesheet" href="/Projectes/01/DawSharing_SleepingOwl/public/css/JFFormStyle-1.css">
+<link type="text/css" rel="stylesheet" href="/Projectes/01/DawSharing_SleepingOwl/public/css/JFGrid.css" />
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
