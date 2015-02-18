1d176c8e361f808030301fdcc6e69cb715a52744
sacer6 <jonatan.d.g@hotmail.com>
2015-02-18 15:50:40 +0100

    cambio href

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -21,7 +21,7 @@
                                 <a role="menuitem" tabindex="-1" href="#">Els meus viatges</a>
                             </li>
                             <li role="presentation">
-                                <a role="menuitem" tabindex="-1" href="missatges">Missatges</a>
+                                <a role="menuitem" tabindex="-1" href="perfil/missatges">Missatges</a>
                             </li>
                             <li role="presentation">
                                 <a role="menuitem" tabindex="-1" href="perfil">Perfil</a>
