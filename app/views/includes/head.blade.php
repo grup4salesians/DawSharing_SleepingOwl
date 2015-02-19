2b767f1fd079edfda60533c44701bb3501e55e8a
sacer6 <jonatan.d.g@hotmail.com>
2015-02-19 15:58:45 +0100

    Ya funciona, como alguien toque algo lo mato!

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -4,21 +4,21 @@
 <link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css'>
 <!--<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>-->
 
-<link href="/DawSharing/public/css/style.css" rel="stylesheet" type="text/css" media="all">
+<link href="<?php echo Config::get('constants.BaseUrl') ?>public/css/style.css" rel="stylesheet" type="text/css" media="all">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--<script src="js/jquery.min.js"></script>-->
 <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script src="/DawSharing/public/js/jquery-ui.js" type="text/javascript"></script>
-<link rel="stylesheet" href="css/jquery-ui.css" >
+<script src="<?php echo Config::get('constants.BaseUrl') ?>public/js/jquery-ui.js" type="text/javascript"></script>
+<link rel="stylesheet" href="<?php echo Config::get('constants.BaseUrl') ?>public/css/jquery-ui.css" >
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-<script src="/DawSharing/public/js/JFCore.js" type="text/javascript"></script>
-<script src="/DawSharing/public/js/JFForms.js" type="text/javascript"></script>
-<link type="text/css" rel="stylesheet" href="/DawSharing/public/css/JFFormStyle-1.css">
-<link type="text/css" rel="stylesheet" href="/DawSharing/public/css/JFGrid.css" />
+<script src="<?php echo Config::get('constants.BaseUrl') ?>public/js/JFCore.js" type="text/javascript"></script>
+<!--<script src="/DawSharing/public/js/JFForms.js" type="text/javascript"></script>-->
+<link type="text/css" rel="stylesheet" href="<?php echo Config::get('constants.BaseUrl') ?>public/css/JFFormStyle-1.css">
+<link type="text/css" rel="stylesheet" href="<?php echo Config::get('constants.BaseUrl') ?>public/css/JFGrid.css" />
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
