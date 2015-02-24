676b3374cc00c1f39dbbe712b4f1fbd5b5d1d950
Vektron <kenzo_aku@hotmail.com>
2015-02-24 19:50:38 +0100

    pas2

---
+++ app/views/includes/publicarViatge/pas2.blade.php
@@ -0,0 +1,29 @@
+<div id="cont-Pas-1-conductor" style="display: none" class="visible testtest">
+    <div id="CampsPas1" class="PublicarViatge_CampsPas1 col-md-6">
+        <div class="form-group">
+            {{ Form::label('frequencia', 'Freqüencia') }}
+            {{ Form::radio('frequencia', 'data_concreta') }}  
+            {{ Form::label('frequencia','Data Concreta') }} 
+            {{ Form::radio('frequencia', 'viatge_periodic') }}  
+            {{ Form::label('frequencia','Viatge Periòdic') }} 
+        </div>
+        <div class="form-group">
+            {{ Form::label('tipus', 'Tipus') }}
+            {{ Form::radio('tipus', 'data_concreta') }}  
+            {{ Form::label('tipus','Data Concreta') }} 
+            {{ Form::radio('tipus', 'viatge_periodic') }}  
+            {{ Form::label('tipus','Viatge Periòdic') }} 
+        </div>   
+        <div class="form-group">
+            {{ Form::label('anda', 'Anada') }}
+            {{ Form::data('searchTextFieldFin',$desti,array('class' => 'PublicarViatge_Elementos','id'=>'searchTextFieldFin')) }} 
+        </div>
+    </div>
+    <div id="Mapa" class="PublicarViatge_Mapa col-md-6">
+        <div id="map_canvas" class="PublicarViatge_MapCanvas"></div>
+        <div id="MapaDistancia">
+            <span class="PublicarViarge_Distancia">  Distància: </span> <div id="distance"> </div> <!--El div distance es para poner la distancia, lo calcula ApiGoogleViatgeDetalls.js-->
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+</div>
