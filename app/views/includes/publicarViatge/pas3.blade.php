a302e5a55d0d67f6ac4b5c3a37922e4b8656b220
sacer6 <jonatan.d.g@hotmail.com>
2015-02-25 16:14:00 +0100

    Revert "pas3"
    This reverts commit e51823bce4d8f76b4a4e2ed275b6e828bf1777ce.

--- app/views/includes/publicarViatge/pas3.blade.php
+++ app/views/includes/publicarViatge/pas3.blade.php
@@ -1,56 +1,35 @@
 <style>
     .Pas3Labels{
         float: left;
-        width: 180px;
+        width: 300px;
     }
     #linea{
         border-top:1px solid black;
         width: 445px;
     }
-    .PublicarViatge_TextBox{
-        width: 60px;
-        margin-left: 40px;
-    }
-    .PublicarViatge_DivSI{
-        margin-right: 70px;
-    }
-    .PublicarViatge_TexArea{
-        margin: 0px;
-        max-width: 430px;
-        max-height: 250px;
-        width: 430px;
-        height: 110px;
-    }
 </style>
 <div id="cont-Pas-3" style="display: none" class="testtest">
-    <div id="CampsPas3" class="PublicarViatge_CampsPas3 col-md-6">
-        <?php
-        $permissos = Caracteristiques::orderBy('id', 'desc')->get();
-        $permis = explode(";", $permissos[0]->permisosViatges);
-        $i = 0;
-        foreach ($permis as &$valor) {
-            ?>
-            <div class="form-group">
-                <div class="Pas3Labels">{{ Form::label($valor, $valor) }} </div>
-                {{ Form::radio('Datos_'+$i, $valor+'_si') }}
-                {{ Form::label('Si', 'Si',array('class'=>'PublicarViatge_DivSI')) }}
-                {{ Form::radio('Datos_'+$i, $valor+'_no') }}
-                {{ Form::label('No', 'No') }}
-
-            </div>
-            <?php
-            $i = $i + 1;
-        }
+    <?php
+    $permissos = Caracteristiques::orderBy('id', 'desc')->get();
+    $permis = explode(";", $permissos[0]->permisosViatges);
+    $i=0;
+    foreach ($permis as &$valor) {
         ?>
-        <div id="linea"></div>
         <div class="form-group">
-            {{ Form::label('NumPlaces', 'Número de places:') }}
-            {{ Form::text('NumPlaces', Input::old('NumPlaces'),array('class' => 'PublicarViatge_TextBox')) }}
-            {{ Form::text('EuroPlaça', Input::old('EuroPlaça'),array('class' => 'PublicarViatge_TextBox')) }}
-            {{ Form::label('EuroPlaça', '€/Plaça') }}
+            <div class="Pas3Labels">{{ Form::label($valor, $valor) }} </div>
+            {{ Form::radio('Datos_'+$i, $valor+'_si') }}
+            {{ Form::label('Si', 'Si') }}
+            {{ Form::radio('Datos_'+$i, $valor+'_no') }}
+            {{ Form::label('No', 'No') }}
+            
         </div>
-        {{ Form::label('Comentaris', 'Comentaris:',array('style'=>'width: 100%;')) }}
-        {{ Form::textarea('comentaris',null,array('class'=>'PublicarViatge_TexArea')) }}
-    </div>
+        <?php
+        $i=$i+1;
+    }
+    ?>
+    <div id="linea"></div>
+    {{ Form::label('NumPlaces', 'Número de places:') }}
+    
+    {{ Form::label('Comentaris', 'Comentaris:') }}
     <div style="clear: both;"></div>
 </div>
