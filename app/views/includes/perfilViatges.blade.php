593c421f0b0ea1f5509286eee6b9c39ae39179d4
Vektron <kenzo_aku@hotmail.com>
2015-03-01 02:45:37 +0100

    Viatges perfil Cutr

--- app/views/includes/perfilViatges.blade.php
+++ app/views/includes/perfilViatges.blade.php
@@ -2,11 +2,13 @@
 <?php
 $passatgers = Passatger::where('usuari_id', Auth::user()->id)->get();
 foreach ($passatgers as $key => $val) {
+	$estatPassatger = $val->estat;
     $viatges = Viatge::where('id', $val->viatge_id)->where('data', '>=', date('d-m-Y'))->orderBy('id', 'desc')->get(); 
-    $ruta = Ruta::where('id', $val->ruta_id)->get();
-    $ruta1 = new blockRuta($val->id, $val->data, $ruta[0]->inici_ruta, $ruta[0]->fi_ruta, $val->preu, $val->numSeientRestant, $val->permissos);
-    echo '<div>';
-    $ruta1->mostrarMapa();
+    $ruta = Ruta::where('id', $viatges[0]->ruta_id)->get();
+    $ruta1 = new blockRuta($viatges[0]->id, $viatges[0]->data, $ruta[0]->inici_ruta, $ruta[0]->fi_ruta, $viatges[0]->preu, $viatges[0]->numSeientRestant, $viatges[0]->permissos);
+    echo '<div style="width: 550px;background-color: rgb(228, 247, 230);">';
+    echo $ruta1->mostrarMapa();
+    echo $estatPassatger;
     echo '</div>';
 }
 ?>
